9783f0b2293770e1465dbb696e09d176dbeddeb3
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-20 13:21:04 -0700

    update

--- main.cpp
+++ main.cpp
@@ -17,6 +17,10 @@ void addDiet(Fish &fish, const string &food);
 
 Fish &addFish(Fish *&fishes, int &count, int &capacity, const string &color, double avgSize);
 
+void printFish(const Fish *fishes, int count);
+
+void freeFish(Fish *fishes, int count);
+
 int main() {
     int numFish = 0;
     cout << "Enter the number of fish caught below, then press enter.";
@@ -46,21 +50,30 @@ void addDiet(Fish &fish, const string &food) {
 }
 
 Fish &addFish(Fish *&fishes, int &count, int &capacity, const string &color, double avgSize){
+    int capacityNew = capacity;
     if (count == capacity) {
-        int capacityNew = capacity;
         if (capacity == 0){
             capacityNew = 2;
         }
         else {
-        newCapacity = fish.dietCapacity *2;
+        capacityNew = capacity *2;
         }
-        string *bigger = new string[newCapacity];
-        for (int i = 0; i <fish.dietCount; i++) {
-        bigger[i] = fish.diet[i];
+        Fish *bigger = new Fish[capacityNew];
+        for (int i = 0; i <capacity; i++) {
+        bigger[i] = fishes[i];
         }
-        delete[] fish.diet;
-        fish.diet = bigger;
-        fish.dietCapacity = newCapacity;
+        delete[] fishes;
+        fishes = bigger;
+        capacity = capacityNew;
     }
+    fishes[count].color = color;
+    fishes[count].avgSize = avgSize;
+    count++;
+    return fishes[count - 1];
+}
+
+void printFish(const Fish *fishes, int count) {
+    for (int i = 0; i < count; i++) {
+        cout << "Average size: " << fishes[i].avgSize << endl; 
     }
 }
