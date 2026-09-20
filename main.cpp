64dd1025d7edae13d6f568e6af806d617299d487
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-20 12:28:35 -0700

    update

--- main.cpp
+++ main.cpp
@@ -3,3 +3,19 @@
 
 using namespace std;
 
+
+struct Fish {
+    double avgSize = 0.0;
+    string color;
+    string* diet = nullptr;
+    int dietCount = 0;
+    int dietCapacity = 0;
+};
+
+addDiet
+
+int main() {
+
+}
+
+
